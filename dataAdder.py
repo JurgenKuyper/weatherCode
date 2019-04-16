00c5433c2710edba3c508712d66559d9f7846691
BuildTools <unconfigured@null.spigotmc.org>
2019-04-16 19:03:01 +0200

    changed some things

--- dataAdder.py
+++ dataAdder.py
@@ -1,24 +1,16 @@
-# url = 'http://192.168.178.10/?JSON'
 import json
 import urllib.request
-with urllib.request.urlopen("http://192.168.178.10/?JSON") as url:
-    data = json.loads(url.read().decode())
-    print(data)
-# weatherList = json.loads(response.read())
-# print(weatherList)
+response = urllib.request.urlopen("http://192.168.178.10/?JSON")
+print(response)
+weatherList = json.load(response.text)
+print(weatherList)
 # jsonDataList = [[],[]]
-# master = Tk()
-# screeny = 25
-# #creating text screen for non-int data
-# text = Text(master)
-# Sbar = Scrollbar(master)
-# Sbar.pack(side=RIGHT, fill=Y)
-# text.pack(side=LEFT, fill=Y)
 # for data in weatherList:
 #         for x in range(0,1):
 #             if data['title'] not in jsonDataList[x]:
 #                 jsonDataList[x].append(data['title'])
 # def getData():
+#     #print(time1[5])
 #     for data in weatherList:
 #         for x in range(0,1):
 #             if data['value'] not in jsonDataList[x]:
